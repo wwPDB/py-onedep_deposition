317edb0d3b0042c5c45303985a7f60f9932d7bce
Neli Fonseca <neli@ebi.ac.uk>
2023-03-30 12:18:59 +0100

    Create all deposition endpoints

--- deposit/deposit_api.py
+++ deposit/deposit_api.py
@@ -28,7 +28,36 @@ class DepositApi:
         experiment = Experiment(exp_type="em", subtype=subtype, related_emdb=related_emdb)
         response = self.create_deposition(email=email, users=users, country=country, experiments=[experiment], password=password)
         return response
-    #TODO: Create specific depositions passing the experiments as a parameter
+
+    def create_xray_deposition(self,  email: str, users : List[str], country : Country, password : str = ""):
+        experiment = Experiment(exp_type="xray")
+        response = self.create_deposition(email=email, users=users, country=country, experiments=[experiment], password=password)
+        return response
+
+    def create_fiber_deposition(self,  email: str, users : List[str], country : Country, password : str = ""):
+        experiment = Experiment(exp_type="fiber")
+        response = self.create_deposition(email=email, users=users, country=country, experiments=[experiment], password=password)
+        return response
+
+    def create_neutron_deposition(self,  email: str, users : List[str], country : Country, password : str = ""):
+        experiment = Experiment(exp_type="neutron")
+        response = self.create_deposition(email=email, users=users, country=country, experiments=[experiment], password=password)
+        return response
+
+    def create_ec_deposition(self,  email: str, users : List[str], country : Country, password : str = "", related_emdb: str = None):
+        experiment = Experiment(exp_type="ec", related_emdb=related_emdb)
+        response = self.create_deposition(email=email, users=users, country=country, experiments=[experiment], password=password)
+        return response
+
+    def create_nmr_deposition(self,  email: str, users : List[str], country : Country, password : str = "", related_bmrb: str = None):
+        experiment = Experiment(exp_type="nmr", related_bmrb=related_bmrb)
+        response = self.create_deposition(email=email, users=users, country=country, experiments=[experiment], password=password)
+        return response
+
+    def create_ssnmr_deposition(self,  email: str, users : List[str], country : Country, password : str = "", related_bmrb: str = None):
+        experiment = Experiment(exp_type="ssnmr", related_bmrb=related_bmrb)
+        response = self.create_deposition(email=email, users=users, country=country, experiments=[experiment], password=password)
+        return response
 
     def get_deposition(self, dep_id: str):
         response = self.rest_adapter.get(f"depositions/{dep_id}")
